309c0bc4220e18b21ffaab2c53325e11a1ecd9db
Shaswat005 <shaswatsurya12@gmail.com>
2025-06-02 12:34:33 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="text-7xl text-green-600 p-6 font-bold">
+      HELLO
+      <h1>hello world</h1>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
